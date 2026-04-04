df517f4291940a346bb0968829ea778c06f45949
susbur439-hash <susbur439@gmail.com>
2026-04-05 00:57:42 +0300

    Delete modules/modules directory

--- modules/modules/decision.py
+++
@@ -1,8 +0,0 @@
-def decision(data):
-    if data["analysis"] == "self_development":
-        data["decision"] = "add_module"
-    else:
-        data["decision"] = "do_nothing"
-
-    data["log"].append("decision made")
-    return data
--- modules/modules/modules/action.py
+++
@@ -1,8 +0,0 @@
-def action(data):
-    if data["decision"] == "add_module":
-        data["result"] = "System wants to add a new module"
-    else:
-        data["result"] = "No action"
-
-    data["log"].append("action selected")
-    return data

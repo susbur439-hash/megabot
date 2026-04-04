fde894440e837e22d94c64d8d5f1fb8fc6572d94
susbur439-hash <susbur439@gmail.com>
2026-04-05 00:28:24 +0300

    Update execution.py

--- modules/execution.py
+++ modules/execution.py
@@ -1,3 +1,9 @@
 def execution(data):
+    print("EXECUTION:", data["result"])
+
+    # 💾 запись в лог (память)
+    with open("logs.txt", "a", encoding="utf-8") as f:
+        f.write(str(data) + "\n")
+
     data["log"].append("execution complete")
     return data

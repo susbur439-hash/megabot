ba7418aaeb346a1ffa003b5cc0218e04a49bf90b
susbur439-hash <susbur439@gmail.com>
2026-04-05 03:00:59 +0300

    Update decision.py

--- modules/decision.py
+++ modules/decision.py
@@ -1,8 +1,15 @@
 def decision(data):
     if data["analysis"] == "self_development":
         data["decision"] = "add_module"
+        data["result"] = "System wants to add a new module"
+
+    elif data["analysis"] == "change_strategy":
+        data["decision"] = "change_strategy"
+        data["result"] = "System changes strategy"
+
     else:
         data["decision"] = "do_nothing"
+        data["result"] = "No action"
 
     data["log"].append("decision made")
     return data
